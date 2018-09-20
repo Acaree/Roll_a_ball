0715ed5d6157c80ff7703cc1142af091ea695bdc
Acaree <acampamar@gmail.com>
2018-09-20 11:42:29 +0200

    Camera and player move

---
+++ Roll-a-Ball/Assets/Scripts/Camera_move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_move : MonoBehaviour {
+	public GameObject player;
+	private Vector3 Camera_Offset;
+	// Use this for initialization
+	void Start () {
+		Camera_Offset = transform.position - player.transform.position;
+
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + Camera_Offset;
+	}
+}
---
+++ Roll-a-Ball/Assets/Scripts/Player_move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player_move : MonoBehaviour {
+	public Rigidbody player_rb;
+	public float speed;
+
+	void Start(){
+		player_rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float Move_h = Input.GetAxis ("Horizontal");
+		float Move_v = Input.GetAxis ("Vertical");
+
+		Vector3 move_vec = new Vector3 (Move_h, 0, Move_v);
+
+		player_rb.AddForce (move_vec * speed); 
+	}
+}
